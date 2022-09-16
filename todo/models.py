b757cf1cef42d8036ed674b76dcd3505519181f5
Kanat Atygayev <atygayev.kanat@gmail.com>
2022-09-17 00:45:35 +0600

    Добавлена модель Todo и зарегистрирована в админ панели

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [
+    ('N', 'Новая'),
+    ('P', 'В процессе'),
+    ('C', 'Выполнена')
+]
+
+class Todo(models.Model):
+    description = models.CharField(verbose_name='Описание', max_length=300, null=False, blank=False)
+    status = models.CharField(verbose_name='Статус', choices=STATUS_CHOICES, max_length=1, null=False, blank=False, default=STATUS_CHOICES[0][0])
+    date = models.DateField(verbose_name='Дата закрытия', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.description} - {self.status}'
